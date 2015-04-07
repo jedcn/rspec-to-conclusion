3c9efce3b6805e78899382b2acb7096b1018f899
Jed Northridge <git@jedcn.com>
2015-04-07 05:58:21 -0400

    Added eslint, started modifying code accordingly

---
+++ test/rspec-runner-test.js
@@ -0,0 +1,48 @@
+var assert = require("assert"),
+    run = require("../lib/rspec-runner").run;
+
+describe("run", function() {
+  "use strict";
+  describe("#run", function() {
+
+    var execArgs,
+        requireArgs,
+        requireResult;
+
+    beforeEach(function() {
+      requireResult = requireArgs = execArgs = null;
+    });
+
+    function execStub() {
+      execArgs = arguments;
+    }
+
+    function requireStub() {
+      requireArgs = arguments;
+      return requireResult;
+    }
+
+    it("runs rspec against 'spec' by default", function() {
+      run("resultsFile", [], execStub, requireStub);
+      assert.ok(execArgs[0].match(/^rspec/));
+      assert.ok(execArgs[0].match(/spec$/));
+    });
+
+    it("runs rspec against the supplied specs", function() {
+      run("resultsFile", ['spec/file1.rb', 'spec/file2.rb'], execStub, requireStub);
+      assert.ok(execArgs[0].match(/^rspec/));
+      assert.ok(execArgs[0].match(/spec\/file1.rb spec\/file2.rb$/));
+    });
+
+    it("puts the results in the file of your choosing", function() {
+      run("resultsForRun1.json", [], execStub, requireStub);
+      assert.ok(execArgs[0].match(/resultsForRun1.json/));
+    });
+
+    it("returns what comes back from requiring in the resultsFile", function() {
+      requireResult = "requireResult";
+      var result = run("resultsFile", [], execStub, requireStub);
+      assert.equal(result, "requireResult");
+    });
+  });
+});
