7f53ff89177f572ce0c26f80bc8ff46f38db2d78
Jed Northridge <git@jedcn.com>
2015-04-07 06:33:54 -0400

    Enhancing ESlint

--- test/rspec-runner-test.js
+++ test/rspec-runner-test.js
@@ -6,48 +6,53 @@ describe("run", function() {
   describe("#run", function() {
 
     var execArgs,
-        requireArgs,
-        requireResult;
+        parseArgs,
+        parseResult;
 
     beforeEach(function() {
-      requireResult = requireArgs = execArgs = null;
+      parseResult = parseArgs = execArgs = null;
     });
 
     function execStub() {
       execArgs = arguments;
     }
 
-    function requireStub() {
-      requireArgs = arguments;
-      return requireResult;
+    function parseStub() {
+      parseArgs = arguments;
+      return parseResult;
     }
 
     it("runs rspec against 'spec' by default", function() {
-      run("resultsFile", [], execStub, requireStub);
+      run("resultsFile", [], execStub, parseStub);
       assert.ok(execArgs[0].match(/^rspec/));
       assert.ok(execArgs[0].match(/spec$/));
     });
 
     it("runs rspec against the supplied specs", function() {
-      run("resultsFile", ['spec/file1.rb', 'spec/file2.rb'], execStub, requireStub);
+      run("resultsFile", ["spec/file1.rb", "spec/file2.rb"], execStub, parseStub);
       assert.ok(execArgs[0].match(/^rspec/));
       assert.ok(execArgs[0].match(/spec\/file1.rb spec\/file2.rb$/));
     });
 
     it("uses the JSON formatter and puts the results in a file", function() {
-      run("resultsForRun1", [], execStub, requireStub);
+      run("resultsForRun1", [], execStub, parseStub);
       assert.ok(execArgs[0].match(/--format json --out resultsForRun1.json/));
     });
 
     it("uses the HTML formatter and puts the results in a file", function() {
-      run("resultsForRun1", [], execStub, requireStub);
+      run("resultsForRun1", [], execStub, parseStub);
       assert.ok(execArgs[0].match(/--format html --out resultsForRun1.html/));
     });
 
-    it("returns what comes back from requiring in the resultsFile", function() {
-      requireResult = "requireResult";
-      var result = run("resultsFile", [], execStub, requireStub);
-      assert.equal(result, "requireResult");
+    it("passes the JSON result file to parse", function() {
+      run("resultsFile", [], execStub, parseStub);
+      assert.ok(parseArgs[0].match(/resultsFile.json/));
+    });
+
+    it("returns JSON that was put into the resultsFile", function() {
+      parseResult = "parseResult";
+      var result = run("resultsFile", [], execStub, parseStub);
+      assert.equal(result, "parseResult");
     });
   });
 });
