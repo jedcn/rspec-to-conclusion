b3aa7c03d8f206b9ef3fb0536f6a126e27004394
Jed Northridge <git@jedcn.com>
2015-04-09 20:42:09 -0400

    Renamed + Cleaned up verify-pre-conditions

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 var buildReport = require("./lib/rspec-reporter").buildReport,
-    table = require("gfm-table");
+    buildVerifyEnv = require("./lib/rspec-verify-env").buildVerifyEnv,
+    table = require("gfm-table"),
+    shelljs = require("shelljs");
 
 module.exports.report = buildReport(table, console);
+module.exports.verifyEnv = buildVerifyEnv(shelljs.which);
 
 module.exports.runToConclusion =
   require("./lib/rspec-to-conclusion").runToConclusion;
-
-module.exports.verifyPreConditions =
-  require("./lib/rspec-pre-conditions").verifyPreConditions;
