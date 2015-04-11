52c7be6bf84f3fd97e779bf9a85d93d2bdedae5b
Jed Northridge <git@jedcn.com>
2015-04-11 10:05:58 -0400

    Make rspec-to-conclusion a "buildable", and clean up rspec-runner

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
 var buildReport = require("./lib/rspec-reporter").buildReport,
+    buildRSpecToConclusion = require("./lib/rspec-to-conclusion").buildRSpecToConclusion,
     buildVerifyArgs = require("./lib/rspec-verify-args").buildVerifyArgs,
     buildVerifyEnv = require("./lib/rspec-verify-env").buildVerifyEnv,
     minimist = require("minimist"),
     shelljs = require("shelljs"),
     table = require("gfm-table");
 
+
 module.exports.report = buildReport(table, console);
 module.exports.verifyEnv = buildVerifyEnv(shelljs.which);
-module.exports.rspecToConclusion =
-  require("./lib/rspec-to-conclusion").rspecToConclusion;
+
+var runDebug = require("debug")("run");
+var rspecRunner = require("./lib/rspec-runner").buildRun(shelljs.exec, require, runDebug);
+module.exports.rspecToConclusion = buildRSpecToConclusion(rspecRunner);
 
 (function() {
 
