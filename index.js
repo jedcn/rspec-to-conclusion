580e6082e7d1aa2b10709db7902055c8c3aad8c4
Jed Northridge <git@jedcn.com>
2015-04-16 19:47:53 -0400

    Rearranged index.js and update run-and-summarize

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-
+// buildXXX methods: creates XXX by injecting dependencies:
 var buildReport = require("./lib/report").buildReport,
     buildRSpecToConclusion = require("./lib/rspec-to-conclusion").buildRSpecToConclusion,
-    buildRunAndSummarize = require("./lib/run-and-summarize").buildRunAndSummarize,
+    buildRunAndMerge = require("./lib/run-and-summarize").buildRunAndMerge,
     buildVerifyArgs = require("./lib/verify-args").buildVerifyArgs,
-    buildVerifyEnv = require("./lib/verify-env").buildVerifyEnv,
-    shelljs = require("shelljs"),
+    buildVerifyEnv = require("./lib/verify-env").buildVerifyEnv;
+
+// External dependencies:
+var shelljs = require("shelljs"),
     table = require("gfm-table");
 
-module.exports.exit = shelljs.exit;
+var runDebug = require("debug")("run");
 
+// Internal dependencies:
+var resultsToProgress = require("./lib/analysis").resultsToProgress,
+    mergeProgress = require("./lib/analysis").mergeProgress,
+    parseArgs = require("./lib/parse-args");
+
+var run = require("./lib/run").buildRun(shelljs.exec, require, runDebug);
+var runAndMerge = buildRunAndMerge(run, resultsToProgress, mergeProgress);
+
+// Exports for explicit use in ./bin/rspec-to-conclusion:
 module.exports.report = buildReport(table, console);
 module.exports.verifyEnv = buildVerifyEnv(shelljs.which);
-
-var runDebug = require("debug")("run");
-var run = require("./lib/run").buildRun(shelljs.exec, require, runDebug);
-var runAndSummarize = buildRunAndSummarize(run);
-
-module.exports.rspecToConclusion = buildRSpecToConclusion(runAndSummarize);
-var parseArgs = require("./lib/parse-args");
+module.exports.rspecToConclusion = buildRSpecToConclusion(runAndMerge);
 module.exports.verifyArgs = buildVerifyArgs(parseArgs);
+module.exports.exit = shelljs.exit;
