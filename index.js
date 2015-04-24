d5e27379f6770b0cbd3f9d584a0f212119bd3f33
Jed Northridge <git@jedcn.com>
2015-04-24 07:45:42 -0400

    Wire everything up in index.js

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-// External dependencies:
-var shelljs = require("shelljs");
+// buildXXX methods: creates XXX by injecting dependencies:
+var buildReport = require("./lib/report"),
+    buildRSpecToConclusion = require("./lib/rspec-to-conclusion"),
+    buildRunAndMerge = require("./lib/run-and-merge"),
+    buildVerifyArgs = require("./lib/verify-args"),
+    buildVerifyEnv = require("./lib/verify-env");
 
+// External dependencies:
+var shelljs = require("shelljs"),
+    table = require("gfm-table");
+
+var runDebug = require("debug")("run");
+
+// Internal dependencies:
+var resultsToProgress = require("./lib/analysis").resultsToProgress,
+    mergeProgress = require("./lib/analysis").mergeProgress,
+    parseArgs = require("./lib/parse-args");
+
+var run = require("./lib/run")(shelljs.exec, require, runDebug);
+var runAndMerge = buildRunAndMerge(run, resultsToProgress, mergeProgress);
+
+// Exports for explicit use in ./bin/rspec-to-conclusion:
+module.exports.report = buildReport(table, console);
+module.exports.verifyEnv = buildVerifyEnv(shelljs.which);
+module.exports.rspecToConclusion = buildRSpecToConclusion(runAndMerge);
+module.exports.verifyArgs = buildVerifyArgs(parseArgs);
 module.exports.exit = shelljs.exit;
